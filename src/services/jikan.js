4f0d03e65386f24c241e761b7c7120d1cd285643
danielz0102 <danyzglez@hotmail.com>
2025-04-10 22:49:07 -0600

    feat: get SNK characters data from Jikan API

---
+++ src/services/jikan.js
@@ -0,0 +1,24 @@
+export async function getCharacters() {
+  let data
+
+  try {
+    const response = await fetch(
+      'https://api.jikan.moe/v4/anime/16498/characters',
+    )
+
+    if (!response.ok) {
+      throw `${response.status} ${response.statusText}`
+    }
+
+    const json = await response.json()
+    data = json.data
+  } catch (error) {
+    console.error('Error fetching characters:', error)
+    return null
+  }
+
+  return data.map(({ character }) => ({
+    name: character.name.split(', ').reverse().join(' '),
+    image: character.images.jpg.image_url,
+  }))
+}
